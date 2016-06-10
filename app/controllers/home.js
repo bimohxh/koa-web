aeb66ef6d0563a33c4fd73fc880b7cea32a1be61
黄小红 <hxh@localhost.(none)>
2016-06-10 20:59:32 +0800

    deal model

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,23 +1,8 @@
-var knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '1234',
-    database : 'webs',
-    charset  : 'utf8'
-  }
-});
-
-var bookshelf = require('bookshelf')(knex);
-var Repo = bookshelf.Model.extend({
-  tableName: 'repos'
-})
-
+const Table = require('../../lib/table')
 
 
 exports.index = async (ctx, next) =>{
-  let repos = await Repo.where('id', 3).fetch()
+  let repos = await Table.Repo.where('id', 3).fetch()
   console.log("ok")
 
   //ctx.body = "hello"
